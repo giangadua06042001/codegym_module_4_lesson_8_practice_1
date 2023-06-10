39736e00d5b34d0e56b933ce369bc39b78b307da
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-06-10 15:42:24 +0700

    bai thuc hanh coppy

---
+++ src/main/java/com/example/practice_1_validation/Practice1ValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_1_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice1ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice1ValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_1_validation/Practice1ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_1_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice1ValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
